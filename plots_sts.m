575bd6398916649084986a7f8d36300a569d6c8b
Jose Faundez-Alarcon <josfa@mech.kth.se>
2020-03-16 18:58:20 +0100

    ...

--- plots_sts.m
+++ plots_sts.m
@@ -19,8 +19,8 @@ uv = data(:,:,11);
 [ny,nx] = size(x);
 
 alpha = zeros(ny,nx);
-dx = x(2,:) - x(1,:);
-dy = y(2,:) - y(1,:);
+dx = x(10,:) - x(1,:);
+dy = y(10,:) - y(1,:);
 L = sqrt(dx.^2 + dy.^2);
 a = acos(dy./L);
 s = zeros(nx,1);
@@ -54,13 +54,6 @@ alpha_deg = alpha_rad*180/pi
 cos_a = cos(alpha);
 sin_a = sin(alpha);
 
-% Compute tangential components
-tt = uu.*cos_a.^2 + vv.*sin_a.^2 + 2*uv.*cos_a.*sin_a;
-T = U.*cos_a + V.*sin_a;
-pp = tt -T.^2;
-pp(abs(pp)<1e-14)=0;
-p = sqrt(pp);
-
 % Create normal and tangential coordinates
 S = zeros(ny,nx);
 N = zeros(ny,nx);
@@ -71,6 +64,51 @@ for n=1:ny
   dh = sqrt(dx.^2+dy.^2);
   N(n,:) = dh;
 end
+
+% Compute tangential components
+tt = uu.*cos_a.^2 + vv.*sin_a.^2 + 2*uv.*cos_a.*sin_a;
+T = U.*cos_a + V.*sin_a;
+pp = tt -T.^2;
+pp(abs(pp)<1e-14)=0;
+p = sqrt(pp);
+
+% Compute normal component
+nn = uu.*sin_a.^2 + vv.*cos_a.^2 - 2*uv.*cos_a.*sin_a;
+Nor = -U.*sin_a + V.*cos_a;
+np2 = nn -Nor.^2;
+np2(abs(np2)<1e-14)=0;
+np = sqrt(np2);
+
+% Compute spanwise component
+zz = ww - W.^2;
+zz(abs(zz)<1e-14)=0;
+zp = sqrt(zz);
+
+u_pert = sqrt((pp + np2 + zz)/3);
+U_av = sqrt(T.^2 + Nor.^2 + W.^2);
+
+
+integrand = u_pert;%./U_av;
+
+dat1 = base_case('fringe_m30.f00008',165,40);
+y99 = dat1.y99;
+TI = zeros(nx,1);
+for i=1:nx
+  for j=1:ny
+    if y99(i)<=N(j,i)
+    	int_i = integrand(j:end-1,i);
+    	n_i = N(j:end-1,i);
+    	TI(i) = trapz(n_i,int_i)/trapz(n_i,ones(length(n_i),1));
+    	break
+    end
+  end
+end
+
+figure()
+plot(S(1,ind:end),TI(ind:end))
+xlim([0, 0.35])
+xlabel('Chord')
+ylabel('Ti')
 
 % Compute cf
 p_max = zeros(nx,1);
@@ -92,7 +130,13 @@ for i=1:nx
   cf1(i) = (-p(1,i) + p(fin,i))/(N(fin,i) - N(1,i))/Re;
   cf2(i) = (-T(1,i) + T(fin,i))/(N(fin,i) - N(1,i))/Re;
 end
-
+figure()
+contourf(S(:,in0:end),N(:,in0:end),u_pert(:,in0:end), 'LineStyle','none')
+xlim([0,0.4])
+ylim([0,0.01])
+ylabel('Normal direction')
+xlabel('Chord')
+colorbar()
 %cf1 = smoothdata(cf1,'Gaussian',10);
 
 figure()
@@ -131,6 +175,14 @@ ylabel('$\frac{1}{Re}\left(\frac{\partial U_T}{\partial n}\right)_{n=0}$',...
 	   'Interpreter','latex', 'FontSize',20)
 xlabel("$Re_s$",'Interpreter','latex','FontSize',16)
 
+figure()
+contourf(S(:,in0:end),N(:,in0:end),np(:,in0:end), 'LineStyle','none')
+xlim([0,0.4])
+ylim([0,0.01])
+ylabel('Normal direction')
+xlabel('Chord')
+title("$\sqrt{\overline{u_{n}'u_{n}'}}$",'Interpreter','latex')
+colorbar()
 
 figure()
 plot(S(1,in0:end), s(in0:end))
